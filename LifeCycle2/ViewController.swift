3419de7ed7c877e7a634c40c7cfcd5e92f557279
eryshkov <wendor265>
2018-09-07 12:51:07 +0300

    Initial Commit

---
+++ LifeCycle2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LifeCycle2
+//
+//  Created by Evgeniy Ryshkov on 07.09.2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
